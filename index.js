72d7bdf4de474c2d30f1efbab66d0729c1d1e17a
Thomas Phillips <thomasphillips3@gmail.com>
2018-05-12 10:02:53 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+    <Text>Word up</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
