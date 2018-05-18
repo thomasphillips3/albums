02d855b9d4853b4600d82162283dc8e06e0f4b19
Thomas Phillips <thomasphillips3@gmail.com>
2018-05-17 18:56:47 -0700

    Display thumbnail image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-    return <Card>
+const AlbumDetail = ({ album }) => {
+
+    const { title, artist, thumbnail_image } = album;
+
+    const styles = {
+        headerContentStyle: {
+            flexDirection: 'column',
+            justifyContent: 'space-around'
+        },
+        thumbnailStyle: {
+            height: 50,
+            width: 50
+        }
+    };
+
+    return (<Card>
         <CardSection>
-          <Text>{props.album.title}</Text>
+          <View>
+                <Image 
+                    style={styles.thumbnailStyle} 
+                    source={{ uri: thumbnail_image }} 
+                />
+          </View>
+          <View style={styles.headerContentStyle}>
+            <Text>{title}</Text>
+            <Text>{artist}</Text>
+          </View>
         </CardSection>
-      </Card>;
+      </Card>
+    );   
 };
 
 export default AlbumDetail;
