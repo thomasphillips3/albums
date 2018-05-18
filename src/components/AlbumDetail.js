b073c8cabc568d269f0bfd1c5bdf5b6e810030b1
Thomas Phillips <thomasphillips3@gmail.com>
2018-05-17 19:43:32 -0700

    Display album image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,8 +4,7 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-
-    const { title, artist, thumbnail_image } = album;
+    const { title, artist, thumbnail_image, image } = album;
 
     const styles = {
         headerContentStyle: {
@@ -15,23 +14,44 @@ const AlbumDetail = ({ album }) => {
         thumbnailStyle: {
             height: 50,
             width: 50
+        },
+        thumbnailContainerStyle: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: 10,
+            marginRight: 10
+        },
+        headerTextStyle: {
+            fontSize: 18
+        },
+        imageStyle: {
+            height: 300,
+            flex: 1,
+            width: null
         }
     };
 
-    return (<Card>
-        <CardSection>
-          <View>
-                <Image 
-                    style={styles.thumbnailStyle} 
-                    source={{ uri: thumbnail_image }} 
+    return (
+        <Card>
+            <CardSection>
+                <View style={styles.thumbnailContainerStyle}>
+                    <Image 
+                        style={styles.thumbnailStyle} 
+                        source={{ uri: thumbnail_image }} 
+                    />
+            </View>
+            <View style={styles.headerContentStyle}>
+                <Text style={styles.headerTextStyle}>{title}</Text>
+                <Text>{artist}</Text>
+            </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    style={styles.imageStyle}
+                    source={{ uri: image }}
                 />
-          </View>
-          <View style={styles.headerContentStyle}>
-            <Text>{title}</Text>
-            <Text>{artist}</Text>
-          </View>
-        </CardSection>
-      </Card>
+            </CardSection>
+        </Card>
     );   
 };
 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
-import AlbumDetail from './AlbumDetail'
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = {albums: [] };
@@ -18,9 +18,9 @@ class AlbumList extends Component {
     render() {
         console.log(this.state);
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
